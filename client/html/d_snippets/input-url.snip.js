2be2e7185a55f04c16bef85039b90263dd428475
se6a <wyss.sebastian@gmail.com>
2021-04-21 18:44:24 +0200

    allow dash in url inputs

--- client/html/d_snippets/input-url.snip.js
+++ client/html/d_snippets/input-url.snip.js
@@ -5,7 +5,7 @@ module.exports = (data) => {
   data.type = "text";
   data.maxlength = 80;
   data.attr = `
-    pattern="(^https?://)?(www\\.)?([^www][a-zA-Z0-9æøåöäüéÆØÅÖÄÜÉ]{2,})(\\.[a-zA-Z]{2,})(/.*)?"
+    pattern="(^https?://)?(www\\.)?([^www][a-zA-Z0-9-æøåöäüéÆØÅÖÄÜÉ]{2,})(\\.[a-zA-Z]{2,})(/.*)?"
   `;
 
   return TextShort(data);
