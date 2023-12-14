d46e030cb3ea1aacd29d018648ac6527ecf728da
se6a <wyss.sebastian@gmail.com>
2023-12-14 16:30:56 +0100

    deactivate livereload

--- server.js
+++ server.js
@@ -17,57 +17,53 @@ global.Server = Express();
 
 /*  Live Reload Server
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-if (ENV.environment === "local") {
-  const LiveReload      = require("livereload");
-  const LRServer        = LiveReload.createServer();
-  const ConnectLRServer = require("connect-livereload");
+// if (ENV.environment === "local") {
+//   const LiveReload      = require("livereload");
+//   const LRServer        = LiveReload.createServer();
+//   const ConnectLRServer = require("connect-livereload");
 
-  LRServer.watch("./client");
-  Server.use(ConnectLRServer());
-}
+//   LRServer.watch("./client");
+//   Server.use(ConnectLRServer());
+// }
 
 /*  Cache
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 async function cacheViews() {
-  const views = await fs.readdirSync(
-    `${__basedir}/client/html/b_views`);
+    const views = await fs.readdirSync(`${__basedir}/client/html/b_views`);
 
-  Server.cache.viewsByName = views.map(
-    (_view) => _view.replace(".view.js", ""));
+    Server.cache.viewsByName = views.map((_view) =>
+        _view.replace(".view.js", "")
+    );
 }
 
 /*  Static Files
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 function staticFiles() {
-  Server.use(
-    Express.static(
-      `${__basedir}/client`,
-      {
-        dotfiles: "ignore",
-        index:    false
-      }
-    )
-  );
+    Server.use(
+        Express.static(`${__basedir}/client`, {
+            dotfiles: "ignore",
+            index: false,
+        })
+    );
 }
 
 /*  Router
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 async function loadRouter() {
-  Server.use("/api/", require("./router/router-api"));
-  Server.use("/", await require("./router/router-client"));
+    Server.use("/api/", require("./router/router-api"));
+    Server.use("/", await require("./router/router-client"));
 }
 
 /*  Start Server
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 async function start() {
-  staticFiles();
-  await cacheViews();
-  loadRouter();
+    staticFiles();
+    await cacheViews();
+    loadRouter();
 
-  Server.listen(
-    ENV.port,
-    () => console.log(`Listening on port ${ENV.port}.`)
-  );
-};
+    Server.listen(ENV.port, () =>
+        console.log(`Listening on port ${ENV.port}.`)
+    );
+}
 
 start();
